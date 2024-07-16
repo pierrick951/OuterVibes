2beb11c8d2bfc9dc7583bc19d5dfaf2c3b073f7e
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-07-16 09:57:50 -0400

    start

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Layout/Home';
+import Error from './Layout/Error';
+function App() {
+
+  return (
+         <BrowserRouter>
+           <Routes>
+                 <Route path='/' element={<Home/>}/>
+                 <Route path='*' element={<Error/>}/>
+           </Routes>
+         
+         </BrowserRouter>
+      
+  )
+}
+
+export default App
---
+++ src/Layout/Error.tsx
@@ -0,0 +1,7 @@
+type Props = {}
+function Error({}: Props) {
+  return (
+    <div>Error</div>
+  )
+}
+export default Error
